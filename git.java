8262e140475711fede90d1e3f81622fd48f95aa0
vaibhav <cs2021183@gobal.org.in>
2025-01-14 15:43:11 +0530

    kjdgjksd

--- git.java
+++ git.java
@@ -2,4 +2,7 @@ public class git{
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+    public static void print(String s){
+        System.out.println("my world");
+    }
 }
